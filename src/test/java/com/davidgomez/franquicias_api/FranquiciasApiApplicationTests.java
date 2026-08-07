26fd1f48d677f212db5e62cf1a55d8b7f54ff129
DavidG0m3z <david.ardilagomez@gmail.com>
2026-08-07 11:55:06 -0500

    Initial commit

---
+++ src/test/java/com/davidgomez/franquicias_api/FranquiciasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davidgomez.franquicias_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FranquiciasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
