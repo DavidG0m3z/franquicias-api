e286d459b6fd895525ab6ee6021339cc28f4f20e
DavidG0m3z <david.ardilagomez@gmail.com>
2026-08-07 15:08:09 -0500

    feat: add domain models and repository gateways

---
+++ src/main/java/com/davidgomez/franquicias_api/domain/model/branch/gateways/BranchRepository.java
@@ -0,0 +1,14 @@
+package com.davidgomez.franquicias_api.domain.model.branch.gateways;
+
+import com.davidgomez.franquicias_api.domain.model.branch.Branch;
+
+import reactor.core.publisher.Mono;
+import reactor.core.publisher.Flux;
+
+public interface BranchRepository {
+
+    Mono<Branch> save(Branch branch);
+    Mono<Branch> findById(String id);
+    Flux<Branch> findByFranchiseId(String franchiseId);
+
+}
