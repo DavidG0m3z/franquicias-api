4aa23ff2c4cdeca9382c903e50cda62c7deca140
DavidG0m3z <david.ardilagomez@gmail.com>
2026-08-09 23:39:45 -0500

    feat: provision MongoDB Atlas with Terraform IaC

---
+++ terraform/variables.tf
@@ -0,0 +1,16 @@
+variable "atlas_org_id" {
+  description = "Organization ID de MongoDB Atlas"
+  type        = string
+}
+
+variable "db_username" {
+  description = "Usuario de la base de datos para la aplicación"
+  type        = string
+  default     = "franquicias_app"
+}
+
+variable "db_password" {
+  description = "Password del usuario de la base de datos"
+  type        = string
+  sensitive   = true
+}
